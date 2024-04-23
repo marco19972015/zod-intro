6816e60da0a1effd1802b3c968618d8514a24d83
Marco Torres <marcoantonio.torres@icloud.com>
2024-04-23 12:19:12 -0400

    In this version I go over some of the basic types we can use in zod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,88 +11,65 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
 
-
     const UserSchema = z.object({
-      username: z.string()
+      username: z.string(),
+      age: z.number(),
+      birthdate: z.date(),
+      isProgrammer: z.boolean()
     })
 
     type User = z.infer<typeof UserSchema>
 
     const user: User = { username: 'Marco'}
 
-    console.log(UserSchema.parse(user));
+    console.log(UserSchema.safeParse(user).success);
     
   }
 
   // In order to use ZOD we import the object z from 'zod'
   // The z object does everything we want to do with zod (no need to worry about anything else)
 
-  // In version 2 - I will go over some basic usage by creating a User schema
+  // In version 3 - In this version we'll go over the differnt basic types we can use (such as primitives)
+  // We can use the following 
+  // z.number()
+  // z.bigint() - if this is going to be a big integer 
+  // z.date() - we know this will be a date
+  // z.boolean() - sets the type to a boolean
 
-  // 1. we create a const called UserSchema and define it as a z.object (equivalent of saing UserSchema: object = {}) 
-  // Therefore, we can infer that we can define other things such as strings, booleans, and so on 
-
-  // 2. we define the object with a username and set it to type zod string
-
-  // 3. We then create a user and create an object defined as username and assign it a 'Marco' as the username
-
-  // 4. We can console.log our Schema and we parse the user. 
-    // This just says, hey does this user match the schema I've defined 
-
-  // In this case it does, so if we check the console.log we can see the user is returned to us, this is good because 
-  // it means it passed the validation
-
-  // However, if const user = { username: 1} is a number instead of a string, we can we imediately get an error 
-  // We get an Uncaught ZodError, this is good because we get errors based on our different validations if things are correct or not 
-
-
-  // Another great thing about zod is the way it integrates with TS
-  // Normally if we have a type we would need to define the user type
-  //            type User = {
-  //              username: string 
-  //            } 
-
-  // We would then define our user, as a type User
-  // const user: User = { username: "Marco"}
-
-  // The issue with this is that we are defining the userSchema twice
-  //            type User = {
-  //              username: string 
-  //            } 
-  // And then 
+  // Currently with the data below if we just log out the success, we'll get false. Because we haven't defined an age, birthday, and isProgrammer 
   //            const UserSchema = z.object({
-  //              username: z.string()
+  //              username: z.string(),
+  //              age: z.number(),
+  //              birthdate: z.date(),
+  //              isProgrammer: z.boolean()
   //            })
 
-  // With zod we don't need to do that. We can instead remove 
-  //            type User = {
-  //              username: string 
-  //            } 
+  //            type User = z.infer<typeof UserSchema>
 
-  // We keep
+  //            const user: User = { username: 'Marco'}
+
+  //            console.log(UserSchema.safeParse(user).success);
+
+  // Inside of ZOD every single validation is required by default
+  
+  
+  // We have the ability to make the validation optional by adding 
+  //            .optional() 
+  // onto the end of a validation
+  // If we added to the fields we can see that success is true
   //            const UserSchema = z.object({
-  //              username: z.string()
+  //              username: z.string().optional(),
+  //              age: z.number().optional(),
+  //              birthdate: z.date().optional(),
+  //              isProgrammer: z.boolean().optional()
   //            })
 
-  // And add 
-  // type User = z.infer<typeof UserSchema>
-  
-  // If we hover over User we can see in line 78 we can see it is a type of username with a string
-  // This means that it automatically inferred the type for us based on our schema, and as we change our schema and 
-  // do different things to it, this type for our users is going to automatically be inferred 
-  // the infer method being a part of zod 
+  // Some other primitive types are undefined, any, or never
+  // z.unefined() - states that value should always be undefined
+  // z.null() - make sure something is null
+  // z.void() - accepts undefined (essentially saying there is nothing)... so return type of a function may be void
+  // z.any() - if we want to accept anything 
+  // z.unknown() - also accepts any type of input
+  // z.never() - if want a key to never be defined (essentially saying it can never have this testkey at all and if it does it will fail)
 
-
-  // final thing we'll cover is controlling our error that is thrown at us when validation doesn't go as planned
-  // We have the option of turning this error into a boolean value using...
-  //            safeParse() function/method
-  // What this does is if it fails it returns us an object which has 
-  //            {success: false, error: ZodError}
-
-  // OR if it passes it returns us 
-  //            {success: true, data:{}}
-  // it returns us the data which is the object itself
-  
-  // This can be useful in a form validation, instead of doing a DB validation
-    
 }
